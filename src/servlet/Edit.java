5efea6be52aee34ff64ddd922e7a0219ee39e740
yuserita <yu.serita@casleyconsulting.co.jp>
2014-05-21 18:42:44 +0900

    Edit修正なう

--- src/servlet/Edit.java
+++ src/servlet/Edit.java
@@ -1,11 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.EditBeans;
 
 /**
  * Servlet implementation class Edit
@@ -13,20 +17,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Edit")
 public class Edit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		beans.EditBeans.DateBaseModify(request);
 		//EditBeans.javaの、データベースに上書きするメソッドを呼び出す。
+		
+		HttpSession session = request.getSession();
+		String ln = request.getParameter("lastname");
+		String fn = request.getParameter("firstname");
+		String sln = request.getParameter("lsubname");
+		String sfn = request.getParameter("fsubname");
+		String bd = request.getParameter("birthday");
+		String pl = request.getParameter("place");
+		String hb = request.getParameter("hobby");
+		String im = request.getParameter("image");
+		String wd = request.getParameter("word");
+		String id = request.getParameter("id");
+
+		session.setAttribute("lastname", request.getParameter("lastname"));
+		session.setAttribute("firstname", request.getParameter("firstname"));
+		session.setAttribute("sublastname", request.getParameter("sublastname"));
+		session.setAttribute("subfirstname",request.getParameter("subfirstname"));
+		session.setAttribute("birthday", request.getParameter("birthday"));
+		session.setAttribute("place", request.getParameter("place"));
+		session.setAttribute("hobby", request.getParameter("hobby"));
+		session.setAttribute("image", request.getParameter("image"));
+		session.setAttribute("word", request.getParameter("word"));
+		session.setAttribute("id", request.getParameter("id"));
+
+		EditBeans editbeans = new EditBeans();
+		editbeans.setFirstname(ln);
+		editbeans.setLastname(fn);
+		editbeans.setFsubname(sln);
+		editbeans.setLsubname(sfn);
+		editbeans.setBirthday(bd);
+		editbeans.setPlace(pl);
+		editbeans.setHobby(hb);
+		editbeans.setImage(im);
+		editbeans.setWord(wd);
+		editbeans.setId(id);
+
+		
+		
 		response.sendRedirect("/syaa/JSP/Top");
 		
 	}
