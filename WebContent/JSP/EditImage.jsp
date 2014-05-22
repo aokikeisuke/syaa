c23f8669f9e4cbe942645a3ffaf00fca8dd2ecb6
yuserita <yu.serita@casleyconsulting.co.jp>
2014-05-22 14:21:16 +0900

    deleteに着手

---
+++ WebContent/JSP/EditImage.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+	
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>編集画面</title>
+</head>
+<body>
+	
+	<form method = "POST" enctype = "multipart/form-data" action = "/syaa/servlet/EditImage">
+	写真：
+	
+	<input type ="file" name="image"  size="75">
+	
+	<br>
+	
+	
+	<input type="submit" value="上書き">
+	</form>
+	
+	
+</body>
+</html>
--- src/servlet/Delete.java
+++ src/servlet/Delete.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import beans.DeleteBeans;
-import beans.EditBeans;
 
 /**
  * Servlet implementation class Edit
@@ -18,26 +17,25 @@ import beans.EditBeans;
 @WebServlet("/Delete")
 public class Delete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		HttpSession session = request.getSession();
-		//IDはint指定ではない？
-		//ここでIDを取得するためにはSearchInBeans.javaで
-		//select *で取得したものをsessionだか何かで送る必要あり。
+		// IDはint指定ではない？
+		// ここでIDを取得するためにはSearchInBeans.javaで
+		// select *で取得したものをsessionだか何かで送る必要あり。
 		String id = request.getParameter("id");
 		session.setAttribute("id", request.getParameter("id"));
 
 		DeleteBeans deletebeans = new DeleteBeans();
 		deletebeans.setId(id);
-		
+
 		beans.DeleteBeans.DateBaseDelete(deletebeans);
-		//EditBeans.javaの、データベースに上書きするメソッドを呼び出す。
+		// EditBeans.javaの、データベースに上書きするメソッドを呼び出す。
 
 		response.sendRedirect("/syaa/JSP/Top");
-		
+
 	}
 
 }
